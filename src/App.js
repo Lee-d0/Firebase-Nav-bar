6b5ffeb68bc7dd4ecce2093beed32cfaeab7c51c
Lee-d0 <marmar11137@gmail.com>
2024-08-04 11:34:42 -0400

    firebaseNavbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { auth }  from './firebase/init'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut , onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({})
+  const [loading, setLoading] = React.useState(true)
+  
+ 
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false)
+      if(user){
+        setUser(user) 
+      } 
+  })
+  }, [])
+  function register(){
+    console.log('register')
+    createUserWithEmailAndPassword(auth,'email@email.com', 'test123')
+    .then((user) => {  
+      console.log(user)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  function logIn(){
+    signInWithEmailAndPassword(auth,'email@email.com', 'test123')
+    .then(({ user }) => {  
+      console.log(user.email[0].toUpperCase())
+      
+      setUser(user)
+    }).catch((error) => {
+      // setErrorMessage('Password or email is incorrect')
+      console.log(error.message)
+    })
+  }
+
+
+
+  function logOut(){
+    signOut(auth)
+    setUser({})
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='img__container'>
+              <figure className='img__wrapper'>
+                <img src='	https://frontendsimplified.com/_nuxt/img/Frontend%20Simplified%20Logo.853fbda.png
+        ' ></img>
+              </figure>
+      </div>
+      <div className='btn__container'>
+      
+        <button onClick={register}>Register</button>
+        <button onClick={logIn}>LogIn</button>
+        {
+          loading ? (<><button onClick={logOut}>LogOut</button></>) : user.email
+        }
+       
+        
+       
+        
+        
+        
+      </div>
+      
     </div>
   );
 }
---
+++ src/firebase/init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPC3wcpp3XFpi5VP-AcXhnlQcZsyq9MY8",
+  authDomain: "fir-practice-6cfb3.firebaseapp.com",
+  projectId: "fir-practice-6cfb3",
+  storageBucket: "fir-practice-6cfb3.appspot.com",
+  messagingSenderId: "771962805502",
+  appId: "1:771962805502:web:1fa7b95ba7631e043099af"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
